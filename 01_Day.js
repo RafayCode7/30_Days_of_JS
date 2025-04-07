4e1391fc09f2bbdfe913b85e551f9137733109d4
Rafia Majeed <rafaycodes17@gmail.com>
2025-04-07 21:20:27 +0500

    30 Days of js , day one

---
+++ 01_Day.js
@@ -0,0 +1,20 @@
+// -------30 Days of Javascript---------
+// Basic and very important command of JS
+console.log("hello world")
+
+// variable declaration 
+let firstname =  'Rafia '
+let lastname =' Majeed'
+let country = 'Pakistan'
+let age = '23'
+let ismarried = false
+
+// Declaring Variables with Number 
+const gravity = 9.18
+const boilingPoint = 100
+const pi = 3.14
+ 
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Rafia', age1 = 23, country1 = 'Pakistan'
+console.log(firstname, lastname, country, age, ismarried) 
