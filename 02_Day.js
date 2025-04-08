fde019a99df30c59fb88f7b6ab1a992c65ec09d8
Rafia Majeed <rafaycodes17@gmail.com>
2025-04-08 20:37:44 +0500

    02 Day of Js

---
+++ 02_Day.js
@@ -0,0 +1,14 @@
+// ------Day Two of Js -------
+// we are going to cover all the concepts of js from the scratch
+// How to add JS in the webpage 
+
+<html>
+  <head>
+    <title>30DaysOfScript:Inline Script</title>
+  </head>
+  <body>
+
+    <h2>Inline</h2>
+    <button onclick="alert('Welcome to 30DaysOfJavaScript!')">Click Me</button>
+  </body>
+</html>
